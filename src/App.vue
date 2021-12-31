24fbad93f6e06adeaff6ff527ee44fa69e203755
kollynce <collinsodeny@gmail.com>
2021-12-31 20:51:51 +0300

    version 1.0

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,105 @@
-<template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+<template class="bg-primary">
+  <header class="text-gray-600 body-font container mx-auto">
+        <div class="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
+            <a class="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0">
+                <svg width="98" height="32" viewBox="0 0 98 32" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M9.895 2.65H16.195V0.399999H9.895C6.421 0.399999 3.595 3.226 3.595 6.7C3.595 10.174 6.421 13 9.895 13H16.195V10.75H9.895C7.663 10.75 5.845 8.932 5.845 6.7C5.845 4.468 7.663 2.65 9.895 2.65ZM23.0727 0.399999C19.5987 0.399999 16.7727 3.226 16.7727 6.7C16.7727 10.174 19.5987 13 23.0727 13C26.5467 13 29.3727 10.174 29.3727 6.7C29.3727 3.226 26.5467 0.399999 23.0727 0.399999ZM23.0727 10.75C20.8407 10.75 19.0227 8.932 19.0227 6.7C19.0227 4.468 20.8407 2.65 23.0727 2.65C25.3047 2.65 27.1227 4.468 27.1227 6.7C27.1227 8.932 25.3047 10.75 23.0727 10.75ZM34.5198 10.75C33.7278 10.642 33.0978 10.012 32.9898 9.22V0.417999H30.7398V9.004C30.7398 9.634 30.8838 10.228 31.1538 10.768C31.5318 11.56 32.1798 12.208 32.9718 12.604C33.5118 12.874 34.1058 13.018 34.7358 13.018H41.1258V10.768L34.5198 10.75ZM46.7015 10.75C45.9095 10.642 45.2795 10.012 45.1715 9.22V0.417999H42.9215V9.004C42.9215 9.634 43.0655 10.228 43.3355 10.768C43.7135 11.56 44.3615 12.208 45.1535 12.604C45.6935 12.874 46.2875 13.018 46.9175 13.018H53.3075V10.768L46.7015 10.75ZM57.3531 13V0.399999H55.1031V13H57.3531ZM69.4961 0.399999V8.428L61.3961 2.182L59.1461 0.454V13.018H61.3961V5.044L69.4961 11.29L71.7461 13.018V0.417999L69.4961 0.399999ZM85.4586 3.874V3.802C85.4586 1.93 83.9286 0.399999 82.0566 0.399999H76.9446C75.0726 0.399999 73.5606 1.93 73.5606 3.802V3.874C73.5606 4.594 73.7766 5.26 74.1546 5.8C74.2806 5.998 74.4246 6.178 74.5866 6.34C75.1986 6.934 76.0266 7.294 76.9446 7.294H81.4806C82.4346 7.294 83.2266 8.068 83.2266 9.022C83.2266 9.976 82.4346 10.75 81.4806 10.75H77.5386C76.5846 10.75 75.8106 9.976 75.8106 9.022H73.5606C73.5606 11.218 75.3426 13 77.5386 13H81.4806C83.6766 13 85.4586 11.218 85.4586 9.022C85.4586 8.068 85.1166 7.186 84.5586 6.502C84.5046 6.448 84.4686 6.394 84.4146 6.34C83.6946 5.548 82.6506 5.044 81.4806 5.044H76.9446C76.3146 5.044 75.7926 4.522 75.7926 3.892V3.82C75.7926 3.19 76.3146 2.668 76.9446 2.668H82.0566C82.6866 2.668 83.2086 3.19 83.2086 3.82V3.892L85.4586 3.874ZM13.5186 18.4H10.1886L6.67855 21.622L5.00455 23.152L3.15055 24.844V18.4H0.900555V31H3.15055V27.886L6.33655 24.988L10.6926 31H13.4646L8.01056 23.458L13.5186 18.4ZM23.685 25.186C24.027 25.15 24.351 25.06 24.657 24.916C25.827 24.376 26.637 23.188 26.637 21.82C26.637 20.884 26.259 20.038 25.647 19.426C25.035 18.814 24.189 18.436 23.253 18.436H15.297V31.036H17.547V25.24H19.797C20.607 25.24 21.309 25.672 21.705 26.32L21.759 26.428L22.965 28.786L24.117 31.036H26.637L25.485 28.786L23.685 25.186ZM17.547 23.008V20.686H23.253C23.649 20.686 23.919 20.866 24.063 21.01C24.189 21.136 24.405 21.424 24.405 21.838C24.405 22.288 24.135 22.702 23.721 22.882C23.577 22.954 23.415 22.99 23.253 22.99L17.547 23.008ZM31.8655 20.65H41.0635V18.4H31.8655C29.9935 18.4 28.4815 19.93 28.4815 21.802V27.598C28.4815 29.47 29.9935 31 31.8655 31H41.0635V28.75H31.8655C31.2355 28.75 30.7135 28.228 30.7135 27.598V25.222H38.8135V22.972H30.7135V21.802C30.7135 21.172 31.2355 20.65 31.8655 20.65ZM53.84 28.75L48.566 18.4L43.292 28.75L42.14 31H44.66L45.812 28.75L48.566 23.35L51.32 28.75L52.472 31H54.992L53.84 28.75ZM52.5104 18.418V20.668H57.6764V31.018H59.9264V20.668H65.0924V18.418H52.5104ZM69.1568 31V18.4H66.9068V31H69.1568ZM76.8178 18.4C73.3438 18.4 70.5178 21.226 70.5178 24.7C70.5178 28.174 73.3438 31 76.8178 31C80.2918 31 83.1178 28.174 83.1178 24.7C83.1178 21.226 80.2918 18.4 76.8178 18.4ZM76.8178 28.75C74.5858 28.75 72.7678 26.932 72.7678 24.7C72.7678 22.468 74.5858 20.65 76.8178 20.65C79.0498 20.65 80.8678 22.468 80.8678 24.7C80.8678 26.932 79.0498 28.75 76.8178 28.75ZM94.835 18.4V26.428L86.735 20.182L84.485 18.454V31.018H86.735V23.044L94.835 29.29L97.085 31.018V18.418L94.835 18.4Z" fill="#794BC4"/>
+                </svg>
+            </a>
+            <nav class="md:ml-auto flex flex-wrap items-center text-base justify-center">
+                <a href="#" class="mr-5 hover:text-primary hover:font-bold">Work</a>
+                <a href="#" class="mr-5 hover:text-primary hover:font-bold">About</a>
+                <a href="#" class="mr-5 hover:text-primary hover:font-bold">Contact</a>
+            </nav>
+        </div>
+    </header>
+    <section>
+        <div class="container mx-auto md:mt-28 mt-5 px-4">
+            <div class="flex justify-center mb-5">
+                <img class="h-40 w-40 rounded-full object-cover" src="https://images.unsplash.com/photo-1534528741775-53994a69daeb?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8OTN8fGZhY2VzfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60" alt="">
+            </div>
+            <div class="flex flex-col justify-center space-y-4">
+                <p class="text-center font-bold text-xl">
+                    Hey! I am Collins Odeny &#128515; <br> UI designer and Fronted Developer
+                </p>
+                <div class="flex justify-center">
+                    <p class="text-center leading-relaxed md:w-1/2">
+                        It is a long established fact that a reader will be distracted by the readable content
+                        of a page when looking at its layout. The point of using Lorem Ipsum is that it has a
+                        more-or-less normal distribution of letters, as opposed to using 'Content here,
+                        content here', making it look like readable English.
+                    </p>
+                </div>
+                <div class="flex justify-center mt-4">
+                    <a class="text-center py-3 px-8 md:bg-primary w-36 md:text-white text-primary font-semibold rounded-2xl hover:underline md:hover:shadow-2xl" href="#">Resume</a>
+                </div>
+                
+            </div>
+        </div>
+    </section>
+    <section>
+        <div class="container mx-auto md:mt-28 mt-10 px-4 space-y-16">
+            <div class="flex flex-col md:flex-row justify-center space-y-4 md:space-y-0 md:space-x-16">
+                <div class="md:w-96 h-96 thumb rounded-2xl flex flex-col items-center justify-center">
+                    <p class="text-center text-white font-bold text-xl">
+                        Homely
+                    </p>
+                    <p class="text-center text-white leading-relaxed px-4">
+                        It is a long established fact that a reader will be distracted by the readable content
+                    </p>
+                </div>
+                <div class="md:w-96 h-96 thumb rounded-2xl flex flex-col items-center justify-center">
+                    <p class="text-center text-white font-bold text-xl">
+                        Homely
+                    </p>
+                    <p class="text-center text-white leading-relaxed px-4">
+                        It is a long established fact that a reader will be distracted by the readable content
+                    </p>
+                </div>
+            </div>
+            <div class="flex flex-col md:flex-row justify-center space-y-4 md:space-y-0 md:space-x-16">
+                <div class="md:w-96 h-96 thumb rounded-2xl flex flex-col items-center justify-center">
+                    <p class="text-center text-white font-bold text-xl">
+                        Homely
+                    </p>
+                    <p class="text-center text-white leading-relaxed px-4">
+                        It is a long established fact that a reader will be distracted by the readable content
+                    </p>
+                </div>
+                <div class="md:w-96 h-96 thumb rounded-2xl flex flex-col items-center justify-center">
+                    <p class="text-center text-white font-bold text-xl">
+                        Homely
+                    </p>
+                    <p class="text-center text-white leading-relaxed px-4">
+                        It is a long established fact that a reader will be distracted by the readable content
+                    </p>
+                </div>
+            </div>
+        </div>
+    </section>
+    <section class="md:container md:mx-auto mt-28 w-full">
+        <div class="flex justify-between md:mx-44 mx-10 border-t-2 border-tertiary">
+            <a href="#" class="text-primary font-medium py-8">Email</a>
+            <a href="#" class="text-primary font-medium py-8">Call</a>
+            <a href="#" class="text-primary font-medium py-8">Instagram</a>
+            <a href="#" class="text-primary font-medium py-8 hidden sm:block">Twitter</a>
+            <a href="#" class="text-primary font-medium py-8 hidden sm:block">GitHub</a>
+            <a href="#" class="text-primary font-medium py-8 hidden sm:block">LinkedIn</a>
+        </div>
+    </section>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
